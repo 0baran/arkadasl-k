527e1d0f8a10d6704b923e9c58cd635531c2ca07
Baran <baran@example.com>
2026-05-23 00:18:12 +0300

    v1.0.26: Fix Android Firebase Crashlytics initialization bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide AuthProvider;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
@@ -24,16 +22,6 @@ void main() async {
         storageBucket: 'anonchat-40b98.firebasestorage.app',
       ),
     );
-
-    // Initialize Crashlytics
-    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
-    PlatformDispatcher.instance.onError = (error, stack) {
-      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-      return true;
-    };
-
-    // Initialize Analytics
-    FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(true);
 
     // Enable Firestore Offline Persistence
     FirebaseFirestore.instance.settings = const Settings(
